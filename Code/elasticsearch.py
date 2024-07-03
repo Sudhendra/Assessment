7ad58138d79c0eecb1e812b784b5bc36f8831c79
Sudhendra <sudhendrakambhamettu@gmail.com>
2024-07-03 16:07:45 -0400

    Create elasticsearch.py

---
+++ Code/elasticsearch.py
@@ -0,0 +1,5 @@
+# TODO:
+# You will write your code here. 
+# Part 1 - Read and process data.
+# Part 2 - Push the formatted data into ES index.
+# part 3 - Retrieve and save the top 10 documents for eahc query.
